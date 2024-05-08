b3b1a490485515c951ef4a4c9c3e675a65ae84d0
ronaldcc25 <racalderon13@utpl.edu.ec>
2024-05-08 16:02:44 -0500

    Problema 1

---
+++ problema1/Problema01/src/paquete1/Ejecutor.java
@@ -0,0 +1,24 @@
+package paquete1;
+
+import paquete2.LibretaNotas;
+
+public class Ejecutor {
+
+    public static void main(String[] args) {
+        
+        /* 
+        Cuando tengamos que calcular algo tenemos que llamar al metodo donde 
+        calculamos dicho valor para poder presentarlo
+        */
+        
+        LibretaNotas libreta1 = new LibretaNotas("Ronald", 10, 8, 9);
+        libreta1.calcularPromedio();
+        
+        LibretaNotas libreta2 = new LibretaNotas("Andres", 8.26, 9, 10);
+        libreta2.calcularPromedio();
+        
+        System.out.printf("%s\n", libreta1);
+        System.out.printf("%s\n", libreta2);
+    }
+
+}
